6d2ee5b24df922ec11a41fdc7a80040ec62a7479
Nada El Kamel <114959945+Nada-elkamel@users.noreply.github.com>
2023-10-11 00:00:16 +0100

    Add files via upload

---
+++ src/app/liste-users/liste-users.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { SerieService } from '../services/serie.service';
+import { User } from '../model/user.model';
+import { Role } from '../model/role.model';
+
+@Component({
+  selector: 'app-liste-users',
+  templateUrl: './liste-users.component.html',
+  styleUrls: ['./liste-users.component.css']
+})
+export class ListeUsersComponent implements OnInit{
+
+  users? : User[]; 
+  roles?: Role[];
+  constructor(public authService: AuthService, private serieService: SerieService) {}
+
+  ngOnInit(): void {
+    this.chargerUsers();
+  }
+
+  chargerUsers(){
+    this.serieService.listeUser().
+    subscribe((users:any)=>{
+    console.log(users);
+    this.users=users;
+    });
+    }
+
+}
